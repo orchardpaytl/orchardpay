dashpay/dash-evo-tool

fix(identity): make Key Info reachable everywhere and keep key labels/holdings consistent (#889) (#945)
* feat(model): add the legacy identity recovery plan and additive merge

Issue #889: a v0.9.3 identity that was already partially loaded before the
upgrade keeps its remaining keys stranded in `data.db`, because the migration
skips a present identity wholesale. That rule is correct — three earlier
reconcile heuristics each failed by inferring user intent from record shape.

`compute_recovery_plan` only lists what the legacy record holds and the modern
one does not, keyed on the `(target, key_id)` map slot and on `None`
associations; it never decides whether an item should come back. That decision
travels back in as an item-level allowlist, so the absence-versus-removal
ambiguity lands with the only party holding the provenance.

`apply_recovery_plan` is additive by construction, not by discipline: it starts
from the fresh modern record, inserts with `or_insert` so the modern copy wins
every collision, recomputes candidacy rather than trusting the allowlist, and
has no write path at all for alias, status, the dpp identity, DPNS names, or
the wallet link. A voting key drags its voter-identity link along, since one
without the other cannot vote.

M1-M10 from the design doc's test matrix. M2/M5/M6/M9/M10 were confirmed RED
against a naive legacy-wins merge before this implementation landed.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

* feat(database): read one legacy identity row through the shared row decoder

Issue #889: the recovery flow needs one identity out of the preserved legacy
`data.db`, not the whole table. A second hand-written reader would be free to
drift from the importer's idea of which rows are readable — and then recovery
could merge from a record the migration never considered importable, or miss
one it did.

So the per-row decode moves into `decode_identity_row` and both readers call
it, along with one shared `SELECT` that binds the sam

tests/kittest/key_info_screen.rs
@@ -1,0 +1,182 @@
+//! Kittest coverage for `KeyInfoScreen`'s arrival refresh.
+//!
+//! The screen persists the whole identity clone it was opened with on every key
+//! add and remove, so a write that lands while it sits on the stack has to be
+//! picked up — otherwise the next ordinary key edit puts the pre-write record
+//! back, silently and with no error to show for it.
+//!
+//! That only holds if the re-read hangs off a hook `AppState` actually
+//! dispatches to a *pushed* screen. `refresh_on_arrival` is not one:
+//! `app.rs` sends it only to root screens, and `KeyInfoScreen` is always pushed
+//! onto the screen stack. This drives the dispatch the running app really makes.
+
+use crate::support::{mount_app, with_isolated_data_dir};
+use dash_evo_tool::app::TaskResult;
+use dash_evo_tool::model::qualified_identity::encrypted_key_storage::{KeyStorage, PrivateKeyData};
+use dash_evo_tool::model::qualified_identity::qualified_identity_public_key::QualifiedIdentityPublicKey;
+use dash_evo_tool::model::qualified_identity::{
+    IdentityStatus, IdentityType, PrivateKeyTarget, QualifiedIdentity,
+};
+use dash_evo_tool::ui::{RootScreenType, Screen};
+use dash_sdk::dpp::identity::accessors::IdentityGettersV0;
+use dash_sdk::dpp::identity::identity_public_key::accessors::v0::IdentityPublicKeyGettersV0;
+use dash_sdk::dpp::identity::{Identity, KeyID, Purpose};
+use dash_sdk::dpp::version::PlatformVersion;
+use dash_sdk::platform::{Identifier, IdentityPublicKey};
+use egui_kittest::kittest::{NodeT, Queryable};
+use std::collections::BTreeMap;
+
+const MAIN: PrivateKeyTarget = PrivateKeyTarget::PrivateKeyOnMainIdentity;
+/// The row control the keys list renders for an authentication key.
+const AUTH_ROW: &str = "Authentication key ›";
+
+/// A key with a chosen id and purpose. Deterministic data, because the row
+/// label this test navigates by is derived from the purpose.
+fn key(id: KeyID, purpose: Purpose) -> IdentityPublicKey {
+    use dash_sdk::dpp::identity::identity_public_key::v0::IdentityPublicKeyV0;
+    use dash_sdk::dpp::identity::{KeyType, SecurityLevel};
+    use dash_sdk::dpp::platform_value::BinaryData;
+    IdentityPublicKeyV0 {
+        id,
+        key_type: KeyType::ECDSA_HASH160,
+        purpose,
+        security_level: SecurityLevel::CRITICAL,
+        read_only: false,
+        data: BinaryData::new(vec![id as u8; 20]),
+        disabled_at: None,
+        contract_bounds: None,
+    }
+    .into()
+}
+
+/// A plain user identity carrying `purposes` as public keys and no private key
+/// material — the state that makes every key openable from the keys list.
+fn identity_with(id_byte: u8, purposes: &[Purpose], alias: &str) -> QualifiedIdentity {
+    let mut identity =
+        Identity::create_basic_identity(Identifier::from([id_byte; 32]), PlatformVersion::latest())
+            .expect("basic identity");
+    for (index, purpose) in purposes.iter().enumerate() {
+        identity.add_public_key(key(index as KeyID, *purpose));
+    }
+    QualifiedIdentity {
+        identity,
+        associated_voter_identity: None,
+        associated_operator_identity: None,
+        associated_owner_key_id: None,
+        identity_type: IdentityType::User,
+        alias: Some(alias.to_string()),
+        private_keys: KeyStorage::default(),
+        dpns_names: vec![],
+        associated_wallets: BTreeMap::new(),
+        secret_access: None,
+        wallet_index: None,
+        top_ups: BTreeMap::new(),
+        status: IdentityStatus::Active,
+        network: dash_sdk::dpp::dashcore::Network::Testnet,
+    }
+}
+
+/// Walk the route a user takes to one key's own page: Identities → Settings →
+/// Advanced → Manage keys → the key.
+fn open_key_info(harness: &mut egui_kittest::Harness<'static, dash_evo_tool::app::AppState>) {
+    // The left nav carries its own "Settings" entry. Both are buttons; only the
+    // nav one is a toggle, so that is what tells the hub tab apart.
+    harness
+        .query_all_by_role_and_label(egui::accesskit::Role::Button, "Settings")
+        .find(|node| node.accesskit_node().toggled().is_none())
+        .expect("the hub must render a Settings tab")
+        .click();
+    harness.run_steps(3);
+    harness.get_by_label("Advanced").click();
+    harness.run_steps(3);
+    harness.get_by_label("Manage keys").click();
+    harness.run_steps(3);
+    harness.get_by_label(AUTH_ROW).click();
+    harness.run_steps(3);
+}
+
+/// A restore that lands while this screen is open never reaches its
+/// `display_task_result` — results go only to the screen that is visible, and a
+/// restore run from elsewhere is not this screen's. Its clone must still be
+/// re-read when the app refreshes it, or the next ordinary key edit writes that
+/// clone back over the restored key.
+///
+/// Drives the refresh the running app actually performs: `AppState::update`
+/// drains `task_result_sender` every frame, and `TaskResult::Refresh` calls
+/// `refresh()` on the *visible* screen — the pushed `KeyInfoScreen`. A screen
+/// that puts its re-read on `refresh_on_arrival` instead is never asked, since
+/// `AppState` sends that hook only to root screens.
+#[test]
+fn a_write_that_lands_while_key_info_is_open_survives_the_next_key_edit() {
+    with_isolated_data_dir(|| {
+        let rt = tokio::runtime::Runtime::new().expect("Failed to create tokio runtime");
+        let _guard = rt.enter();
+
+        let mut harness = mount_app(RootScreenType::RootScreenIdentityHub);
+        // The Settings tab stacks two columns above the Advanced expander; a
+        // short window puts the keys controls below the fold, where a simulated
+        // click lands outside the widget.
+        harness.set_size(egui::vec2(1280.0, 1800.0));
+        let app_context = harness.state().current_app_context().clone();
+        let identity = identity_with(0x51, &[Purpose::AUTHENTICATION], "arrival-refresh");
+        let identity_id = identity.identity.id();
+        app_context
+            .insert_local_qualified_identity(&identity, &None)
+            .expect("seed the identity");
+        harness.run_steps(5);
+
+        open_key_info(&mut harness);
+        assert!(
+            matches!(
+                harness.state().screen_stack.last(),
+                Some(Screen::KeyInfoScreen(_))
+            ),
+            "the premise: the key's own page is the visible screen"
+        );
+
+        // The restore lands behind this screen's back: the record gains a key
+        // this screen's clone knows nothing about.
+        let restored = key(1, Purpose::TRANSFER);
+        let mut record = app_context
+            .get_local_qualified_identity(&identity_id)
+            .expect("read the record")
+            .expect("record stored");
+        record.identity.add_public_key(restored.clone());
+        record.private_keys.private_keys.insert(
+            (MAIN, restored.id()),
+            (
+                QualifiedIdentityPublicKey::from(restored.clone()),
+                PrivateKeyData::Clear([0x22; 32]),
+            ),
+        );
+        app_context
+            .update_local_qualified_identity(&record)
+            .expect("the other writer's write");
+
+        harness
+            .state()
+            .task_result_sender
+            .try_send(TaskResult::Refresh)
+            .expect("queue the refresh the app dispatches");
+        harness.run_steps(3);
+
+        // What every key add and remove on this screen does with its clone.
+        let Some(Screen::KeyInfoScreen(screen)) = harness.state().screen_stack.last() else {
+            panic!("Key Info must still be the open screen");
+        };
+        app_context
+            .update_local_qualified_identity(&screen.identity)
+            .expect("the next key edit's write");
+
+        assert!(
+            app_context
+                .get_local_qualified_identity(&identity_id)
+                .expect("read back")
+                .expect("still stored")
+                .private_keys
+                .private_keys
+                .contains_key(&(MAIN, restored.id())),
+            "a key edit on this screen must not erase a key written while it was open",
+        );
+    });
+}

tests/kittest/legacy_recovery_section.rs
@@ -9,6 +9,7 @@ use dash_evo_tool::model::legacy_recovery::{
 use dash_evo_tool::model::qualified_identity::PrivateKeyTarget;
 use dash_evo_tool::ui::components::legacy_recovery_section::LegacyRecoverySection;
 use dash_evo_tool::ui::components::{Component, ComponentResponse};
+use dash_evo_tool::ui::masternodes::KeyVocabulary;
 use dash_sdk::dpp::identity::Purpose;
 use egui_kittest::Harness;
 use egui_kittest::kittest::Queryable;
@@ -52,7 +53,7 @@ fn render(
     let mut harness = Harness::builder()
         .with_size(egui::vec2(600.0, 400.0))
         .build_ui(move |ui| {
-            let response = LegacyRecoverySection::new(&plan)
+            let response = LegacyRecoverySection::new(&plan, KeyVocabulary::Masternode)
                 .restoring(restoring)
                 .show(ui);
             if let Some(items) = response.inner.changed_value() {

tests/kittest/transfer_screen.rs
@@ -1,0 +1,91 @@
+//! Kittest coverage for the transfer screen's no-transfer-key branch.
+//!
+//! The branch exists to say "you cannot send from this identity". It used to
+//! offer only "Add key" — typing a private key by hand — which is the wrong
+//! remedy for an identity whose keys are merely stranded in the previous
+//! version's saved data. It now also signposts the keys list, where those keys
+//! can be brought back. The gate deciding the branch is deliberately untouched:
+//! it still correctly reports that the identity cannot send.
+
+use crate::support::{fresh_app_context, with_isolated_data_dir};
+use dash_evo_tool::app::AppAction;
+use dash_evo_tool::model::qualified_identity::encrypted_key_storage::KeyStorage;
+use dash_evo_tool::model::qualified_identity::{IdentityStatus, IdentityType, QualifiedIdentity};
+use dash_evo_tool::ui::identities::transfer_screen::TransferScreen;
+use dash_evo_tool::ui::{Screen, ScreenLike};
+use dash_sdk::dpp::identity::Identity;
+use dash_sdk::dpp::version::PlatformVersion;
+use dash_sdk::platform::Identifier;
+use egui_kittest::Harness;
+use egui_kittest::kittest::Queryable;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::rc::Rc;
+
+/// An identity with no transfer key loaded, which is what puts the transfer
+/// screen into the branch under test.
+fn identity_without_transfer_keys() -> QualifiedIdentity {
+    let identity =
+        Identity::create_basic_identity(Identifier::from([0x51u8; 32]), PlatformVersion::latest())
+            .expect("basic identity");
+    QualifiedIdentity {
+        identity,
+        associated_voter_identity: None,
+        associated_operator_identity: None,
+        associated_owner_key_id: None,
+        identity_type: IdentityType::User,
+        alias: Some("no-transfer-key".to_string()),
+        private_keys: KeyStorage::default(),
+        dpns_names: vec![],
+        associated_wallets: BTreeMap::new(),
+        secret_access: None,
+        wallet_index: None,
+        top_ups: BTreeMap::new(),
+        status: IdentityStatus::Active,
+        network: dash_sdk::dpp::dashcore::Network::Testnet,
+    }
+}
+
+/// The no-key branch signposts the keys list instead of dead-ending on "Add
+/// key", and clicking it opens that list.
+#[test]
+fn the_no_transfer_key_branch_routes_to_the_keys_list() {
+    with_isolated_data_dir(|| {
+        let (_rt, app_context) = fresh_app_context();
+        let mut screen = TransferScreen::new(identity_without_transfer_keys(), &app_context);
+
+        let action = Rc::new(RefCell::new(AppAction::None));
+        let capture = action.clone();
+        let mut harness = Harness::builder()
+            .with_size(egui::vec2(1280.0, 900.0))
+            .build_ui(move |ui| {
+                let act = screen.ui(ui);
+                if act != AppAction::None {
+                    *capture.borrow_mut() = act;
+                }
+            });
+        harness.run_steps(3);
+
+        // The premise: this identity cannot send, so the branch is on screen.
+        assert!(
+            harness.query_by_label("Add key").is_some(),
+            "an identity with no transfer key must be in the no-key branch"
+        );
+
+        assert!(
+            harness.query_by_label("Manage keys").is_some(),
+            "the no-key branch must offer a way to see this identity's keys"
+        );
+
+        harness.get_by_label("Manage keys").click();
+        harness.run_steps(2);
+
+        assert!(
+            matches!(
+                &*action.borrow(),
+                AppAction::AddScreen(Screen::KeysScreen(_))
+            ),
+            "the signpost must open the keys list, where a stranded key can be restored"
+        );
+    });
+}

orchardpaytl/orchardpay

tests/kittest/key_info_screen.rs
@@ -1,0 +1,182 @@
+//! Kittest coverage for `KeyInfoScreen`'s arrival refresh.
+//!
+//! The screen persists the whole identity clone it was opened with on every key
+//! add and remove, so a write that lands while it sits on the stack has to be
+//! picked up — otherwise the next ordinary key edit puts the pre-write record
+//! back, silently and with no error to show for it.
+//!
+//! That only holds if the re-read hangs off a hook `AppState` actually
+//! dispatches to a *pushed* screen. `refresh_on_arrival` is not one:
+//! `app.rs` sends it only to root screens, and `KeyInfoScreen` is always pushed
+//! onto the screen stack. This drives the dispatch the running app really makes.
+
+use crate::support::{mount_app, with_isolated_data_dir};
+use orchardpay::app::TaskResult;
+use orchardpay::model::qualified_identity::encrypted_key_storage::{KeyStorage, PrivateKeyData};
+use orchardpay::model::qualified_identity::qualified_identity_public_key::QualifiedIdentityPublicKey;
+use orchardpay::model::qualified_identity::{
+    IdentityStatus, IdentityType, PrivateKeyTarget, QualifiedIdentity,
+};
+use orchardpay::ui::{RootScreenType, Screen};
+use dash_sdk::dpp::identity::accessors::IdentityGettersV0;
+use dash_sdk::dpp::identity::identity_public_key::accessors::v0::IdentityPublicKeyGettersV0;
+use dash_sdk::dpp::identity::{Identity, KeyID, Purpose};
+use dash_sdk::dpp::version::PlatformVersion;
+use dash_sdk::platform::{Identifier, IdentityPublicKey};
+use egui_kittest::kittest::{NodeT, Queryable};
+use std::collections::BTreeMap;
+
+const MAIN: PrivateKeyTarget = PrivateKeyTarget::PrivateKeyOnMainIdentity;
+/// The row control the keys list renders for an authentication key.
+const AUTH_ROW: &str = "Authentication key ›";
+
+/// A key with a chosen id and purpose. Deterministic data, because the row
+/// label this test navigates by is derived from the purpose.
+fn key(id: KeyID, purpose: Purpose) -> IdentityPublicKey {
+    use dash_sdk::dpp::identity::identity_public_key::v0::IdentityPublicKeyV0;
+    use dash_sdk::dpp::identity::{KeyType, SecurityLevel};
+    use dash_sdk::dpp::platform_value::BinaryData;
+    IdentityPublicKeyV0 {
+        id,
+        key_type: KeyType::ECDSA_HASH160,
+        purpose,
+        security_level: SecurityLevel::CRITICAL,
+        read_only: false,
+        data: BinaryData::new(vec![id as u8; 20]),
+        disabled_at: None,
+        contract_bounds: None,
+    }
+    .into()
+}
+
+/// A plain user identity carrying `purposes` as public keys and no private key
+/// material — the state that makes every key openable from the keys list.
+fn identity_with(id_byte: u8, purposes: &[Purpose], alias: &str) -> QualifiedIdentity {
+    let mut identity =
+        Identity::create_basic_identity(Identifier::from([id_byte; 32]), PlatformVersion::latest())
+            .expect("basic identity");
+    for (index, purpose) in purposes.iter().enumerate() {
+        identity.add_public_key(key(index as KeyID, *purpose));
+    }
+    QualifiedIdentity {
+        identity,
+        associated_voter_identity: None,
+        associated_operator_identity: None,
+        associated_owner_key_id: None,
+        identity_type: IdentityType::User,
+        alias: Some(alias.to_string()),
+        private_keys: KeyStorage::default(),
+        dpns_names: vec![],
+        associated_wallets: BTreeMap::new(),
+        secret_access: None,
+        wallet_index: None,
+        top_ups: BTreeMap::new(),
+        status: IdentityStatus::Active,
+        network: dash_sdk::dpp::dashcore::Network::Testnet,
+    }
+}
+
+/// Walk the route a user takes to one key's own page: Identities → Settings →
+/// Advanced → Manage keys → the key.
+fn open_key_info(harness: &mut egui_kittest::Harness<'static, orchardpay::app::AppState>) {
+    // The left nav carries its own "Settings" entry. Both are buttons; only the
+    // nav one is a toggle, so that is what tells the hub tab apart.
+    harness
+        .query_all_by_role_and_label(egui::accesskit::Role::Button, "Settings")
+        .find(|node| node.accesskit_node().toggled().is_none())
+        .expect("the hub must render a Settings tab")
+        .click();
+    harness.run_steps(3);
+    harness.get_by_label("Advanced").click();
+    harness.run_steps(3);
+    harness.get_by_label("Manage keys").click();
+    harness.run_steps(3);
+    harness.get_by_label(AUTH_ROW).click();
+    harness.run_steps(3);
+}
+
+/// A restore that lands while this screen is open never reaches its
+/// `display_task_result` — results go only to the screen that is visible, and a
+/// restore run from elsewhere is not this screen's. Its clone must still be
+/// re-read when the app refreshes it, or the next ordinary key edit writes that
+/// clone back over the restored key.
+///
+/// Drives the refresh the running app actually performs: `AppState::update`
+/// drains `task_result_sender` every frame, and `TaskResult::Refresh` calls
+/// `refresh()` on the *visible* screen — the pushed `KeyInfoScreen`. A screen
+/// that puts its re-read on `refresh_on_arrival` instead is never asked, since
+/// `AppState` sends that hook only to root screens.
+#[test]
+fn a_write_that_lands_while_key_info_is_open_survives_the_next_key_edit() {
+    with_isolated_data_dir(|| {
+        let rt = tokio::runtime::Runtime::new().expect("Failed to create tokio runtime");
+        let _guard = rt.enter();
+
+        let mut harness = mount_app(RootScreenType::RootScreenIdentityHub);
+        // The Settings tab stacks two columns above the Advanced expander; a
+        // short window puts the keys controls below the fold, where a simulated
+        // click lands outside the widget.
+        harness.set_size(egui::vec2(1280.0, 1800.0));
+        let app_context = harness.state().current_app_context().clone();
+        let identity = identity_with(0x51, &[Purpose::AUTHENTICATION], "arrival-refresh");
+        let identity_id = identity.identity.id();
+        app_context
+            .insert_local_qualified_identity(&identity, &None)
+            .expect("seed the identity");
+        harness.run_steps(5);
+
+        open_key_info(&mut harness);
+        assert!(
+            matches!(
+                harness.state().screen_stack.last(),
+                Some(Screen::KeyInfoScreen(_))
+            ),
+            "the premise: the key's own page is the visible screen"
+        );
+
+        // The restore lands behind this screen's back: the record gains a key
+        // this screen's clone knows nothing about.
+        let restored = key(1, Purpose::TRANSFER);
+        let mut record = app_context
+            .get_local_qualified_identity(&identity_id)
+            .expect("read the record")
+            .expect("record stored");
+        record.identity.add_public_key(restored.clone());
+        record.private_keys.private_keys.insert(
+            (MAIN, restored.id()),
+            (
+                QualifiedIdentityPublicKey::from(restored.clone()),
+                PrivateKeyData::Clear([0x22; 32]),
+            ),
+        );
+        app_context
+            .update_local_qualified_identity(&record)
+            .expect("the other writer's write");
+
+        harness
+            .state()
+            .task_result_sender
+            .try_send(TaskResult::Refresh)
+            .expect("queue the refresh the app dispatches");
+        harness.run_steps(3);
+
+        // What every key add and remove on this screen does with its clone.
+        let Some(Screen::KeyInfoScreen(screen)) = harness.state().screen_stack.last() else {
+            panic!("Key Info must still be the open screen");
+        };
+        app_context
+            .update_local_qualified_identity(&screen.identity)
+            .expect("the next key edit's write");
+
+        assert!(
+            app_context
+                .get_local_qualified_identity(&identity_id)
+                .expect("read back")
+                .expect("still stored")
+                .private_keys
+                .private_keys
+                .contains_key(&(MAIN, restored.id())),
+            "a key edit on this screen must not erase a key written while it was open",
+        );
+    });
+}

tests/kittest/legacy_recovery_section.rs
@@ -12,6 +12,7 @@ use orchardpay::model::legacy_recovery::{
 use orchardpay::model::qualified_identity::PrivateKeyTarget;
 use orchardpay::ui::components::legacy_recovery_section::LegacyRecoverySection;
 use orchardpay::ui::components::{Component, ComponentResponse};
+use orchardpay::ui::masternodes::KeyVocabulary;
 
 const INTRO: &str = "Some keys for this identity from your previous Dash Evo Tool version haven't been brought \
      across.";
@@ -52,7 +53,7 @@ fn render(
     let mut harness = Harness::builder()
         .with_size(egui::vec2(600.0, 400.0))
         .build_ui(move |ui| {
-            let response = LegacyRecoverySection::new(&plan)
+            let response = LegacyRecoverySection::new(&plan, KeyVocabulary::Masternode)
                 .restoring(restoring)
                 .show(ui);
             if let Some(items) = response.inner.changed_value() {

tests/kittest/transfer_screen.rs
@@ -1,0 +1,91 @@
+//! Kittest coverage for the transfer screen's no-transfer-key branch.
+//!
+//! The branch exists to say "you cannot send from this identity". It used to
+//! offer only "Add key" — typing a private key by hand — which is the wrong
+//! remedy for an identity whose keys are merely stranded in the previous
+//! version's saved data. It now also signposts the keys list, where those keys
+//! can be brought back. The gate deciding the branch is deliberately untouched:
+//! it still correctly reports that the identity cannot send.
+
+use crate::support::{fresh_app_context, with_isolated_data_dir};
+use orchardpay::app::AppAction;
+use orchardpay::model::qualified_identity::encrypted_key_storage::KeyStorage;
+use orchardpay::model::qualified_identity::{IdentityStatus, IdentityType, QualifiedIdentity};
+use orchardpay::ui::identities::transfer_screen::TransferScreen;
+use orchardpay::ui::{Screen, ScreenLike};
+use dash_sdk::dpp::identity::Identity;
+use dash_sdk::dpp::version::PlatformVersion;
+use dash_sdk::platform::Identifier;
+use egui_kittest::Harness;
+use egui_kittest::kittest::Queryable;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::rc::Rc;
+
+/// An identity with no transfer key loaded, which is what puts the transfer
+/// screen into the branch under test.
+fn identity_without_transfer_keys() -> QualifiedIdentity {
+    let identity =
+        Identity::create_basic_identity(Identifier::from([0x51u8; 32]), PlatformVersion::latest())
+            .expect("basic identity");
+    QualifiedIdentity {
+        identity,
+        associated_voter_identity: None,
+        associated_operator_identity: None,
+        associated_owner_key_id: None,
+        identity_type: IdentityType::User,
+        alias: Some("no-transfer-key".to_string()),
+        private_keys: KeyStorage::default(),
+        dpns_names: vec![],
+        associated_wallets: BTreeMap::new(),
+        secret_access: None,
+        wallet_index: None,
+        top_ups: BTreeMap::new(),
+        status: IdentityStatus::Active,
+        network: dash_sdk::dpp::dashcore::Network::Testnet,
+    }
+}
+
+/// The no-key branch signposts the keys list instead of dead-ending on "Add
+/// key", and clicking it opens that list.
+#[test]
+fn the_no_transfer_key_branch_routes_to_the_keys_list() {
+    with_isolated_data_dir(|| {
+        let (_rt, app_context) = fresh_app_context();
+        let mut screen = TransferScreen::new(identity_without_transfer_keys(), &app_context);
+
+        let action = Rc::new(RefCell::new(AppAction::None));
+        let capture = action.clone();
+        let mut harness = Harness::builder()
+            .with_size(egui::vec2(1280.0, 900.0))
+            .build_ui(move |ui| {
+                let act = screen.ui(ui);
+                if act != AppAction::None {
+                    *capture.borrow_mut() = act;
+                }
+            });
+        harness.run_steps(3);
+
+        // The premise: this identity cannot send, so the branch is on screen.
+        assert!(
+            harness.query_by_label("Add key").is_some(),
+            "an identity with no transfer key must be in the no-key branch"
+        );
+
+        assert!(
+            harness.query_by_label("Manage keys").is_some(),
+            "the no-key branch must offer a way to see this identity's keys"
+        );
+
+        harness.get_by_label("Manage keys").click();
+        harness.run_steps(2);
+
+        assert!(
+            matches!(
+                &*action.borrow(),
+                AppAction::AddScreen(Screen::KeysScreen(_))
+            ),
+            "the signpost must open the keys list, where a stranded key can be restored"
+        );
+    });
+}